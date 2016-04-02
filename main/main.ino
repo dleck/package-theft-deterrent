bef94cd98c9dd4d9f7cb629bab742d9ebd0ae412
jkhoh <jkhoh@ucsd.edu>
2016-04-02 01:26:10 -0700

    update checkpackage()

--- main/main.ino
+++ main/main.ino
@@ -7,6 +7,8 @@
 #define BUZZER_PIN 0 //TODO: SET LATER
 #define FORCE_PIN 22 //PB00
 
+#define FORCE_RATIO_THRESHOLD 1.5
+
 /*
  * Capacitive Touchpad
  * MPR121 -> SAM D21
@@ -18,7 +20,8 @@
  */
  
 
-int forceReading = 0;
+int newForceReading = 0;
+int oldForceReading = 0;
 boolean isArmed = false;
 
 void setup() {
@@ -59,8 +62,16 @@ void checkKeypadCode() {
 
 void checkPackages() {
   // check new force resistor value
+  oldForceReading = newForceReading;
+  newForceReading = analogRead(FORCE_PIN);
+  
   // compare force values
   // update force value OR trigger theft alarm
+
+  // trigger alarm
+  if ( float(newForceReading/oldForceReading) > FORCE_RATIO_THRESHOLD ) {
+    digitalWrite(BUZZER_PIN, HIGH);
+  }
 }
 
 
