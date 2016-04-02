97ed4f26b74e32674c3d3c6767152083e2baa66b
Darren Eck <darreneck@sbcglobal.net>
2016-04-02 00:58:55 -0700

    added touchpad connection details

--- main/main.ino
+++ main/main.ino
@@ -7,6 +7,17 @@
 #define BUZZER_PIN 0 //TODO: SET LATER
 #define FORCE_PIN 22 //PB00
 
+/*
+ * Capacitive Touchpad
+ * MPR121 -> SAM D21
+ * GND -> GND
+ * VCC -> VCC (3.3V)
+ * SDA -> PA08
+ * SCL -> PA09
+ * IRQ -> PB04
+ */
+ 
+
 int forceReading = 0;
 
 void setup() {
