d5704067aa5cda06226b10408367318499c08d28
Darren Eck <darreneck@sbcglobal.net>
2016-04-02 02:13:45 -0700

    Added Touchpad Code Setting, and .gitignore file

--- main/main.ino
+++ main/main.ino
@@ -4,8 +4,26 @@
  * 
  */
 
+
+/* begin Touchpad Initializations */
+#include <Wire.h>
+#include "Adafruit_MPR121.h"
+#define _BV(bit) (1 << (bit))   // added for compatibility
+
+// You can have up to 4 on one i2c bus but one is enough!
+Adafruit_MPR121 cap = Adafruit_MPR121();
+
+// Keeps track of the last pins touched
+// so we know when buttons are 'released'
+uint16_t lasttouched = 0;
+uint16_t currtouched = 0;
+/* end Touchpad Initializations */
+
+
 #define BUZZER_PIN 0 //TODO: SET LATER
 #define FORCE_PIN 22 //PB00
+
+#define CODE_LENGTH 4
 
 /*
  * Capacitive Touchpad
@@ -21,10 +39,24 @@
 int forceReading = 0;
 boolean isArmed = false;
 
+int code[CODE_LENGTH] = {0, 0, 0, 0};
+int codeCount = 0;
+
 void setup() {
+  while (!Serial);        // needed to keep from starting too fast
   Serial.begin(115200);
+  
   // pin initialization
-  pinMode(FORCE_PIN, INPUT);
+  // pinMode(FORCE_PIN, INPUT);
+
+  // touchpad initialization
+  // Default address is 0x5A, if tied to 3.3V its 0x5B
+  // If tied to SDA its 0x5C and if SCL then 0x5D
+  if (!cap.begin(0x5A)) {
+    Serial.println("MPR121 not found");
+    while (1);
+  }
+  Serial.println("MPR121 found!");
 
 }
 
@@ -47,6 +79,42 @@ void setCode() {
   // keep track of code entered
   // record code after enter key is hit
   // FINALLY arm device
+
+  // Get the currently touched pads
+  currtouched = cap.touched();
+
+  // TODO: solve for case where user presses multiple buttons SUPER SIMULANEOUSLY
+  for (uint8_t i=0; i<12; i++) {
+    // it if *is* touched and *wasnt* touched before, alert!
+    if ((currtouched & _BV(i)) && !(lasttouched & _BV(i)) && lasttouched == 0) {
+      Serial.print(i); Serial.println(" touched");
+      code[codeCount] = i;
+      codeCount++;
+    }
+    // if it *was* touched and now *isnt*, alert!
+    if (!(currtouched & _BV(i)) && (lasttouched & _BV(i)) ) {
+      Serial.print(i); Serial.println(" released");
+    }
+  }
+
+  // reset our state
+  lasttouched = currtouched;
+
+  // full code entered, arm device
+  if (!(codeCount < 4)) {
+    // wait for all buttons to be released
+//    while(lasttouched != 0);
+
+    // arm device
+    isArmed = true;
+    Serial.println("Device Armed!");
+    Serial.print("Code: ");
+    for (int i=0; i<CODE_LENGTH; i++) {
+      Serial.print(code[i]);
+    }
+    Serial.println();
+  }
+  
 }
 
 void checkKeypadCode() {
@@ -66,4 +134,6 @@ void checkPackages() {
 
 
 
+
+/*------------------------- TOUCHPAD FUNCTIONS------------------- */
 
